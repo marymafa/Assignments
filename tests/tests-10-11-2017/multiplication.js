820ed65d16e4baf9b1e3b4bb9c3da5b186cc3038
marymafa <marymafa@gmail.com>
2017-11-10 12:16:05 +0000

    modifoied a file

--- tests/tests-10-11-2017/multiplication.js
+++ tests/tests-10-11-2017/multiplication.js
@@ -2,14 +2,9 @@
 function multiply(num1, num2) {
   var numbersArray = [];
   numbersArray.push(num1,num2);
-
    return numbersArray.reduce(function(a,b){
       return a * b;
   });
 }
-
-var output = multiply(3, 3);
-console.log(output);
-
-var output = multiply((3), (3));
-console.log(output);
+console.log(multiply(3,3));
+console.log(multiply((3),(3)));
