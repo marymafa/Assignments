88a2fb578d2e46d3db02b8177d5833eebe692f9c
marymafa <marymafa@gmail.com>
2017-11-10 09:31:51 +0000

    added code

---
+++ tests/tests-10-11-2017/brag.js
@@ -0,0 +1,17 @@
+function bragFirst()
+
+{
+  return {
+      brag: "Ska ba forgiva!"
+  };
+}
+
+function bragAgain()
+{
+  return
+  {
+      brag: "Ska ba forgiva!"
+  };
+}
+//console.log(bragFirst());
+console.log(bragAgain());
