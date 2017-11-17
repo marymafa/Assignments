f4f8685a31fbb293f9b295f88a52210dbee3e214
marymafa <marymafa@gmail.com>
2017-11-17 11:04:49 +0000

    made changes

--- tests/tests-17-11-2017/moreki.js
+++ tests/tests-17-11-2017/moreki.js
@@ -9,3 +9,8 @@ var age = 27;
 };
 
 moreki();
+
+
+This  code will output  undefined because  this function is void  meaning that do not return  a value after the function executes.
+The variable after the console log is a local variable  that means it references in the function in which it is declared  overide  the same
+variable  name in the large scope.
