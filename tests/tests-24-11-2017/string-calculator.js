7a6d73fed572ebf22792385011747634996ae179
marymafa <marymafa@gmail.com>
2017-11-24 14:22:26 +0000

    made changes

--- tests/tests-24-11-2017/string-calculator.js
+++ tests/tests-24-11-2017/string-calculator.js
@@ -1,24 +1,18 @@
 function int_Add(str) {
     var array = [];
     var replaceArr = str.replace(/\n/g,",");
-    for(var i in array){
-        if(array[i]=== "\n"){
-            str.replace(/\n/g,",");
-        }
-        return str;
-    }
 
-    // / for (var i = 0; i < replceArr.length; i++) {
+    // for (var i = 0; i < replceArr.length; i++) {
     //     array.push(replceArr[i]);
-    //     if (replceArr[i] == "\n") {
-    //        sum += 0;
+    //     if (replceArr[i] == "") {
+    //         return 0;
 
     //     }
     // }
     // var finalAnswer = array.reduce(function (a, r) {
     //     return parseInt(a) + parseInt(r);
     // });
-    //return replaceArr;
+    return replaceArr;
 
 }
 
@@ -26,3 +20,4 @@ console.log(int_Add(""));
 console.log(int_Add("1"));
 console.log(int_Add("1\n2,3"));
 console.log(int_Add("1,2"));
+
