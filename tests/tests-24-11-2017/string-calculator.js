04e3b371e83d52f077377eea3b8552d84d97d392
marymafa <marymafa@gmail.com>
2017-11-24 13:10:20 +0000

    added files with new code

--- tests/tests-24-11-2017/string-calculator.js
+++ tests/tests-24-11-2017/string-calculator.js
@@ -1,18 +1,28 @@
-function addInt(str) {
-    var newArr = [];
-    var splittedStr = str.split(",");
-
-
-    for(var i=0; i<splittedStr.length; i++){
-        newArr.push(splittedStr[i]);
-        if(newArr== ""){
-        return 0;
-    } 
+function int_Add(str) {
+    var array = [];
+    var replaceArr = str.replace(/\n/g,",");
+    for(var i in array){
+        if(array[i]=== "\n"){
+            str.replace(/\n/g,",");
+        }
+        return str;
     }
-    return newArr.reduce(function(a,r){
-        return a + r;
-    });
+
+    // / for (var i = 0; i < replceArr.length; i++) {
+    //     array.push(replceArr[i]);
+    //     if (replceArr[i] == "\n") {
+    //        sum += 0;
+
+    //     }
+    // }
+    // var finalAnswer = array.reduce(function (a, r) {
+    //     return parseInt(a) + parseInt(r);
+    // });
+    //return replaceArr;
+
 }
-console.log(addInt("1,2"));
-console.log(addInt("3,5"));
-console.log(addInt(""));
+
+console.log(int_Add(""));
+console.log(int_Add("1"));
+console.log(int_Add("1\n2,3"));
+console.log(int_Add("1,2"));
