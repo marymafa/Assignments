946f95f1f05810ef407302b2f207d01d8c78db81
marymafa <marymafa@gmail.com>
2017-11-29 15:25:03 +0000

    added a new file and modified a file

--- tests/tests-24-11-2017/string-calculator.js
+++ tests/tests-24-11-2017/string-calculator.js
@@ -1,11 +1,12 @@
 function addInt(numbers) {
     var sum = 0;
-
-    // var regex = (/^-?[0-9]\d*(\.\d+)?$/);
     var numlistNewLine = numbers.replace(/[\n]/g, ',');
     var splitted = numlistNewLine.split(',');
     if (splitted == "") {
         return 0;
+    }else{
+        if(/(\d).(\d).exec(numbers) || (-\d).exec(numbers) /);
+        var numbers=numbers.match(/(-\d)|(\d)[.](\d)|([0-9])/g);
     }
     for (var i = 0; i < splitted.length; i++) {
         if (splitted.Length == 1) {
@@ -13,7 +14,7 @@ function addInt(numbers) {
         }
         sum = splitted.reduce(function (a, b) {
              if (parseInt(a)  < 0 || parseInt(b) < 0) {
-            return new Error(`{sorry negatives not allowed}`);
+            return new Error(`sorry negatives not allowed: ${numbers}`);
         }
 
             return parseInt(a) + parseInt(b);
