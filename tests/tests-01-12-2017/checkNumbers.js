374e0322ed0bd1a608b49a2f1acf2aa669bdf658
marymafa <marymafa@gmail.com>
2017-12-01 11:54:23 +0000

    added new file

---
+++ tests/tests-01-12-2017/checkNumbers.js
@@ -0,0 +1,13 @@
+function checkNumbers(array, number, string) {
+    var newArr = [];
+    var i = 0;
+    if (string === "GreaterOrEqual" && array[i] >= number) {
+        newArr.push(array[i]);
+    } if (string === "LessThan" && array[i] < number) {
+        newArr.push(array[i])
+    }
+
+    return newArr;
+}
+console.log(checkNumbers([1, 2, 3, 4, 5, 6, 7], 3, "GreaterOrEqual"));//[3,4,5,6,7]
+console.log(checkNumbers([1, 2, 3, 4, 5, 6, 7], 5, "LessThan"));//[1,2,3,4]
