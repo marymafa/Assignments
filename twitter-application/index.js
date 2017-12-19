fd562e1f5b9321c9b9c52468ec38123f80a06a2f
marymafa <marymafa@gmail.com>
2017-12-19 08:22:50 +0000

    made changes

--- twitter-application/index.js
+++ twitter-application/index.js
@@ -16,9 +16,74 @@ access_point = {
     url: url,
     oauth: oauth,
     qs: {
-        status: "It is working"
+        status: "It's working"
     }
 };
+
+exports.register = function(req,res){
+  // console.log("req",req.body);
+  var today = new Date();
+  var users={
+    "first_name":req.body.first_name,
+    "last_name":req.body.last_name,
+    "email":req.body.email,
+    "password":req.body.password,
+    "created":today,
+    "modified":today
+  }
+  connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+  if (error) {
+    console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    console.log('The solution is: ', results);
+    res.send({
+      "code":200,
+      "success":"user registered sucessfully"
+        });
+  }
+  });
+}
+
+exports.login = function(req,res){
+  var email= req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+  if (error) {
+    // console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    // console.log('The solution is: ', results);
+    if(results.length >0){
+      if([0].password == password){
+        res.send({
+          "code":200,
+          "success":"login sucessfull"
+            });
+      }
+      else{
+        res.send({
+          "code":204,
+          "success":"Email and password does not match"
+            });
+      }
+    }
+    else{
+      res.send({
+        "code":204,
+        "success":"Email does not exits"
+          });
+    }
+  }
+  });
+}
+
 
 request.post(access_point, function (err, res, body) {
     console.log("http res code", res.statusCode)
