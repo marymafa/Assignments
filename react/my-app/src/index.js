1d6779028a125fdd3ea86eb8b261b29f6ba79da2
marymafa <marymafa80@gmail.com>
2018-03-08 16:09:23 +0200

    made changes

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -3,25 +3,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+// class Square extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: null,
+//     };
+//   }
+
+
+//   render() {
+//     return (
+//       <div>
+//         <button className="square" onClick={() => this.setState({ value: 'x' })}>{this.state.value} </button>
+//       </div>
+//     );
+//   }
+
+// }
+
+
+// ReactDOM.render(<Square />, document.getElementById("root"));
+
+
+
+class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      lastName: '',
+      firstName: '',
     };
   }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
-      </button>
+
+
+      <form>
+        <label>
+          Firstname:
+    <input type="text" name="firstname" placeholder="firstname" />
+        </label><br />
+        <label>
+          Lastname:
+    <input type="text" name="lastname" placeholder="lastname" />
+        </label><br />
+        <label>
+          ContactNo:
+    <input type="text" name="contactno" placeholder="contactno" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+
     );
   }
-  
 }
-
-
-
-ReactDOM.render(<Square />, document.getElementById("root"));
+ReactDOM.render(<Form />, document.getElementById("root"))
 
